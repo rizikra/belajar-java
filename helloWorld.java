6e8aae6ae725bf1f64b0ccc2f53b9ebb3606812c
Rizik <rizikardi01@gmail.com>
2019-12-05 13:18:34 +0700

    belajar java

--- helloWorld.java
+++ helloWorld.java
@@ -1,5 +1,5 @@
 public class helloWorld{
-		public static void main (String args []){
+		public static void main (String[] args){
 			System.out.println("Hello Dunia");
 			mobil bmw = new mobil();//kita bangun object mobil bmw
 			bmw.merk = "BMW i3000";
